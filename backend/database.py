cab2916903cf4f438d0cfcdfa0eb8dc359d0d92f
atman-onit <atman.bharadwaj06@gmail.com>
2026-08-20 13:00:19 +0530

    changes in database.py added functions and minor tweaks

--- backend/database.py
+++ backend/database.py
@@ -1,23 +1,32 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
-
-from config import DATABASE_URL
-
-
-class Base(DeclarativeBase):
-    pass
+from sqlalchemy.pool import StaticPool
+from .config import DATABASE_URL
+from .models import Base
 
 
+#engine start
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+    poolclass = StaticPool
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args=connect_args,
+        poolclass=poolclass,
+        echo=False
+    )
+else:
+    engine = create_engine(DATABASE_URL, echo=False)
 
-connect_args = {"check_same_thread": False} if "sqlite" in DATABASE_URL else{}
-
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
-
+#session factory
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+#functions-
+def create_all_tables():
+    Base.metadata.create_all(bind=engine)
+    print(f"✓ Database tables created")
 
 def get_db():
-    
     db = SessionLocal()
     try:
         yield db
@@ -25,7 +34,10 @@ def get_db():
         db.close()
 
 
-def init_db():
-    
-    import models 
-    Base.metadata.create_all(bind=engine)
+async def init_db():
+    create_all_tables() #on startup
+
+
+async def close_db():
+    engine.dispose() #on shutdown
+    print("✓ Database connection closed")
